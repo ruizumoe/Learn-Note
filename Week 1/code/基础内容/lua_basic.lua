de4b01dcb829d1c55a1f78184fea44c35f26d879
ruizumoe <122783785@qq.com>
2023-07-21 14:51:30 +0800

    写至运算符

--- Week 1/code/基础内容/lua_basic.lua
+++ Week 1/code/基础内容/lua_basic.lua
@@ -48,6 +48,74 @@ testFun(table1,
 
 -- 变量
 -- 测试值传递
+
 local v_test_1, v_test_2 = 0, 3
 v_test_1, v_test_2 = v_test_1 + 1, v_test_1 + 1
 print(v_test_1, v_test_2)       --   ---> 1, 1
+
+
+-- 循环
+
+-- while循环
+local while_condition = 10;
+while (while_condition < 15)
+do
+    print(while_condition)
+    while_condition = while_condition + 1
+end
+
+-- for循环 （测试同名）
+
+local for_condition = 10
+function f(x)
+    return x + 2;
+end
+
+for for_condition = 8, f(for_condition), 1
+do
+    print(for_condition)
+end
+
+-- for 循环 泛型遍历
+local for_ipairs = { key1 = "val1", key2 = "val2" }
+for k, v in pairs(for_ipairs)
+do
+    print(k .. "::" .. v)
+end
+
+
+-- 函数 可变参数
+local function average_table(...)
+    local result = 0;
+    local arg = { ... }   -- 将可变参数转化为一个tab
+    for i, v in ipairs(arg)     -- 等价于ipairs({...})
+    do
+        result = result + v
+    end    
+    -- select("#", ...) 等价于 #arg
+    return result / #arg    -- #arg表示求table的数据个数
+end
+
+print("平均值为", average_table(1, 2, 3, 4, 5, 6))
+
+-- 函数 select
+print(select("#", 1, 3, nil, 5, 9, 11))
+
+-- 运算符
+local op_table1 = {}
+op_table1[1] = "1"
+op_table1[5] = 10
+op_table1[6] = nil
+print(#op_table1)
+
+
+tab5 = {}
+tab5[1] = "1"
+tab5[2] = nil
+tab5[3] = "2"
+tab5[4] = "3"
+tab5[5] = "3"
+tab5[6] = nil
+tab5[7] = "3"
+tab5[7] = "3"
+print("tab5的长度", #tab5)
---
+++ Week 1/code/基础内容/tempCodeRunnerFile.lua
@@ -0,0 +1,10 @@
+tab5 = {}
+tab5[1] = "1"
+tab5[2] = nil
+tab5[3] = "2"
+tab5[4] = "3"
+tab5[5] = "3"
+tab5[6] = nil
+tab5[7] = "3"
+tab5[7] = "3"
+print("tab5的长度", #tab5)
